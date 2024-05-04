a72df12110b42bd08e996dce2901b4fbf076c2b8
Negar <negarsaljooghi@gmail.com>
2024-05-04 18:27:18 +0330

    add JSON data

--- sandbox.js
+++ sandbox.js
@@ -2,20 +2,17 @@ const getTodos = (callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener("readystatechange", () => {
-        //console.log(request, request.readyState)
         if (request.readyState === 4 && request.status === 200) {
-            callback(undefined, request.responseText);
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data);
         } else if (request.readyState === 4) {
             callback('could not fetch data', undefined);
         }
     });
 
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.open('GET', 'todos.json');
     request.send();
 };
-
-console.log(1);
-console.log(2);
 
 getTodos((err, data) => {
     console.log('callback fired');
@@ -25,7 +22,4 @@ getTodos((err, data) => {
         console.log(data);
     }
 });
-
-console.log(3);
-console.log(4);
 
