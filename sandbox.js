b84de61fe143bf2aeae80b56694e56779e5aabf3
Negar <negarsaljooghi@gmail.com>
2024-05-05 15:04:23 +0330

    Promise basics

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,24 @@
-const getTodos = (resourse, callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resourse) => {
 
-    request.addEventListener("readystatechange", () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText)
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback('could not fetch data', undefined);
-        }
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+
+        request.addEventListener("readystatechange", () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText)
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting resource')
+            }
+        });
+
+        request.open('GET', resourse);
+        request.send();
     });
-
-    request.open('GET', resourse);
-    request.send();
 };
 
-getTodos('todos/luigi.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/mario.json', (err, data) => {
-        console.log(data);
-        getTodos('todos/shaun.json', (err, data) => {
-            console.log(data);
-        });
-    });
-});
-
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise resolved:', data);
+}).catch(err => {
+    console.log('promise rejected:', err);
+})
