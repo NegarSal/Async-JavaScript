3211dcd278265294e5e373c9611328cdb7a64ace
Negar <negarsaljooghi@gmail.com>
2024-05-05 16:05:22 +0330

    Throwing & catching errors

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,15 @@
 // async & await
 
 const getTodos = async () => {
-    const response = await fetch('todos/luigi.json');
+    const response = await fetch('todos/luigis.json');
+
+    if (response.status !== 200) {
+        throw new Error('can not fetch the data');
+    }
     const data = await response.json();
     return data;
 };
-
-console.log(1);
-console.log(2);
-
 getTodos()
-    .then(data => console.log('resolved', data));
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
 
-console.log(3);
-console.log(4);
-
-
-
-// fetch api
-
-// fetch('todos/luigi.json').then((response) => {
-//     console.log('resolved', response);
-//     return response.json();
-// }).then(data => {
-//     console.log(data);
-// }).catch((err) => {
-//     console.log('rejected', err);
-// });
